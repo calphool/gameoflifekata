478d39a0e375842bc0c5e762e2103b47d00dfff8
calphool <jrounceville@hotmail.com>
2016-07-12 21:11:04 -0500

    test GOL grid knows its sizes

--- src/com/rounceville/GameOfLife/GameOfLife.java
+++ src/com/rounceville/GameOfLife/GameOfLife.java
@@ -2,10 +2,24 @@ package com.rounceville.GameOfLife;
 
 public class GameOfLife {
 
+	int iMyRowSize;
+	int iMyColumnSize;
+	
 	public GameOfLife(int iRowSize, int iColumnSize) {
 		if(iRowSize < 1)
 			throw new IllegalArgumentException("Invalid row size");
 		if(iColumnSize < 1)
 			throw new IllegalArgumentException("Invalid column size");
+		iMyColumnSize = iColumnSize;
+		iMyRowSize = iRowSize;
 	}
+
+	public int getRowSize() {
+		return iMyRowSize;
+	}
+	
+	public int getColumnSize() {
+		return iMyColumnSize;
+	}
+
 }
--- src/com/rounceville/GameOfLife/GameOfLifeTest.java
+++ src/com/rounceville/GameOfLife/GameOfLifeTest.java
@@ -1,44 +1,51 @@
 package com.rounceville.GameOfLife;
+  
+ import static org.junit.Assert.*;
+  
+ import org.junit.Rule;
+ import org.junit.Test;
+ import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-public class GameOfLifeTest {
-
-	@Rule
-	public final ExpectedException exception = ExpectedException.none();
-		
-	@Test
-	public void testThatInvalidSizedGridsCannotBeCreatedTest1() {
+ public class GameOfLifeTest {
+ 
+ 	@Rule
+ 	public final ExpectedException exception = ExpectedException.none();
+ 		
+ 	@Test
+ 	public void testThatInvalidSizedGridsCannotBeCreatedTest1() {
+ 		exception.expect(IllegalArgumentException.class);
+ 		new GameOfLife(0, 1);
+ 	}
+ 
+ 	@Test
+ 	public void testThatInvalidSizedGridsCannotBeCreatedTest2() {
+ 		exception.expect(IllegalArgumentException.class);
+ 		new GameOfLife(1, 0);
+ 	}
+ 
+ 	@Test
+ 	public void testThatInvalidSizedGridsCannotBeCreatedTest3() {
+ 		exception.expect(IllegalArgumentException.class);
+ 		new GameOfLife(-1, 0);
+  	}
+  
+ 	@Test
+ 	public void testThatInvalidSizedGridsCannotBeCreatedTest4() {
 		exception.expect(IllegalArgumentException.class);
-		new GameOfLife(0, 1);
-	}
+ 		new GameOfLife(0, -1);
+  	}
+ 
+ 	@Test
+ 	public void testGameOfLifeConstructsAnObject() {
+ 		GameOfLife gol = new GameOfLife(1, 1);
+ 		assertNotEquals(gol, null);
+  	}
 
-	@Test
-	public void testThatInvalidSizedGridsCannotBeCreatedTest2() {
-		exception.expect(IllegalArgumentException.class);
-		new GameOfLife(1, 0);
-	}
-
-	@Test
-	public void testThatInvalidSizedGridsCannotBeCreatedTest3() {
-		exception.expect(IllegalArgumentException.class);
-		new GameOfLife(-1, 0);
-	}
-
-	@Test
-	public void testThatInvalidSizedGridsCannotBeCreatedTest4() {
-		exception.expect(IllegalArgumentException.class);
-		new GameOfLife(0, -1);
-	}
-
-	@Test
-	public void testGameOfLifeConstructsAnObject() {
-		GameOfLife gol = new GameOfLife(1, 1);
-		assertNotEquals(gol, null);
-	}
-
-}
+ 	@Test
+ 	public void testGameOfLifeRemembersItsSizes() {
+ 		GameOfLife gol = new GameOfLife(1, 2);
+ 		assertEquals(1, gol.getRowSize());
+ 		assertEquals(2, gol.getColumnSize());
+  	}
+  
+  }
